1fd4ec1b325e008da5f4df3010e9a0fdcd2e032d
Murad <agariomadman@gmail.com>
2019-04-02 15:57:08 -0500

    Did a little more optimization

--- index.js
+++ index.js
@@ -74,26 +74,21 @@ bot.on("message", function (msg) {
                 var json = JSON.parse(text)
                 difference = jso.items[0].statistics.subscriberCount - json.items[0].statistics.subscriberCount
                 // msg.channel.send(`\`\`\`java\n PewDiePie: ` + PewSubs + `\n T-Series: ` + TSubs + `\n Difference: ` + difference + `\n\`\`\``)
+                var lead;
                 if (jso.items[0].statistics.subscriberCount > json.items[0].statistics.subscriberCount) {
-                    var tweetlink = "https://twitter.com/intent/tweet?text=PewDiePie%20is%20winning%20the%20war%20against%20T-Series%20with%20a%20difference%20of%20" + numberWithCommas(difference) + "!%20PewDiePie%20is%20currently%20at%20" + numberWithCommas(jso.items[0].statistics.subscriberCount) + "%20while%20T-Series%20is%20at%20" + numberWithCommas(json.items[0].statistics.subscriberCount) + "!%20Keep%20it%20up,%20we%20got%20this!%20%23creatorsnotcorporations%20%23subtopewdiepie%20%23pewdiebot"
-                    TinyURL.shorten(tweetlink, function (res) {
-                        tweetlink = res
-                    })
-                    const embed = new Discord.RichEmbed()
-                        .setTitle("Tweet about it")
-                        .setAuthor(bot.user.username, bot.user.avatarURL)
-                        .setColor("#f44e42")
-                        // .setDescription("")
-                        .setFooter("", bot.user.avatarURL)
-                        .setTimestamp()
-                        .setURL(tweetlink)
-                        .addField("PewDiePie\"s subcount",
-                            numberWithCommas(jso.items[0].statistics.subscriberCount))
-                        .addField("T-Series\"s subcount", numberWithCommas(json.items[0].statistics.subscriberCount))
-                        .addField("Difference between", numberWithCommas(difference) + " with PewDiePie in the lead")
-                    msg.channel.send({ embed })
+                    lead = "PewDiePie"
                 } else {
-                    var tweetlink = "https://twitter.com/intent/tweet?text=PewDiePie%20is%20losing%20the%20war%20against%20T-Series%20with%20a%20difference%20of%20" + numberWithCommas(difference) + "!%20PewDiePie%20is%20currently%20at%20" + numberWithCommas(jso.items[0].statistics.subscriberCount) + "%20while%20T-Series%20is%20at%20" + numberWithCommas(json.items[0].statistics.subscriberCount) + "!%20We%20still%20have%20a%20chance,%20do%20whatever%20you%20can%20to%20get%20PewDiePie%20more%20subs.%20%23creatorsnotcorporations%20%23subtopewdiepie%20%23pewdiebot"
+                    lead = "T-Series"
+                }
+                    var PewWin = "https://twitter.com/intent/tweet?text=PewDiePie%20is%20winning%20the%20war%20against%20T-Series%20with%20a%20difference%20of%20" + numberWithCommas(difference) + "!%20PewDiePie%20is%20currently%20at%20" + numberWithCommas(jso.items[0].statistics.subscriberCount) + "%20while%20T-Series%20is%20at%20" + numberWithCommas(json.items[0].statistics.subscriberCount) + "!%20Keep%20it%20up,%20we%20got%20this!%20%23creatorsnotcorporations%20%23subtopewdiepie%20%23pewdiebot"
+                    var Twin = "https://twitter.com/intent/tweet?text=PewDiePie%20is%20losing%20the%20war%20against%20T-Series%20with%20a%20difference%20of%20" + numberWithCommas(difference) + "!%20PewDiePie%20is%20currently%20at%20" + numberWithCommas(jso.items[0].statistics.subscriberCount) + "%20while%20T-Series%20is%20at%20" + numberWithCommas(json.items[0].statistics.subscriberCount) + "!%20We%20still%20have%20a%20chance,%20do%20whatever%20you%20can%20to%20get%20PewDiePie%20more%20subs.%20%23creatorsnotcorporations%20%23subtopewdiepie%20%23pewdiebot"
+                    function leadTweet(){
+                    if(jso.items[0].statistics.subscriberCount > json.items[0].statistics.subscriberCount){
+                        return PewWin 
+                    } else {
+                        return Twin 
+                    }
+                    }
                     const embed = new Discord.RichEmbed()
                         .setTitle("Tweet about it")
                         .setAuthor(bot.user.username, bot.user.avatarURL)
@@ -101,16 +96,15 @@ bot.on("message", function (msg) {
                         // .setDescription("")
                         .setFooter("", bot.user.avatarURL)
                         .setTimestamp()
-                        .setURL(tweetlink)
-                        .addField("PewDiePie\"s subcount",
+                        .setURL(leadTweet())
+                        .addField("PewDiePie\'s subcount",
                             numberWithCommas(jso.items[0].statistics.subscriberCount))
-                        .addField("T-Series\"s subcount", numberWithCommas(json.items[0].statistics.subscriberCount))
-                        .addField("Difference between", numberWithCommas(difference) + " with T-Series in the lead")
+                        .addField("T-Series\'s subcount", numberWithCommas(json.items[0].statistics.subscriberCount))
+                        .addField("Difference between", numberWithCommas(difference) + " with " + lead + " in the lead")
                     msg.channel.send({ embed })
-                }
+                })
             })
-        })
-    }
+        }
 
     // If the user used a banned word, take action
     if (checkForBannedWords(input)) {
